940136545dbf7b54f88738acea104260b04ce1d3
fQwQf <supertjz123@foxmail.com>
2024-10-28 12:56:32 +0800

    2-28

---
+++ 2024_10_21/2-28-2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main() {
+    int num = 2; // 从2开始检查
+    do {
+        bool isPrime = true;
+        int divisor = 2;
+        do {
+            if (num % divisor == 0) {
+                isPrime = false;
+                break;
+            }
+            divisor++;
+        } while (divisor * divisor <= num);
+        if (isPrime) {
+            std::cout << num << " ";
+        }
+        num++;
+    } while (num <= 100);
+    return 0;
+}
